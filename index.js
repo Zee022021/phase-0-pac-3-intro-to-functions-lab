4aa225ec2c630e7bbf337d1773d61ebd3bb1c68c
ZiaulhaqHaqyar <fnu.ziaulhaq@flatironschool.com>
2022-01-25 10:59:22 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+    return string.toUpperCase("Hello");
+}
+
+function whisper(string) {
+    return string.toLowerCase("Hello")
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase("hello"));
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase('hello'));
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    string = string.toLowerCase();
+    string = string.toUpperCase();
+    return "I can't hear you!", "YES INDEED!";
+}
+
